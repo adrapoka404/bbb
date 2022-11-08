44581f743a6e7969fdce51fba78e850cb88c11ae
adrapoka404 <adrapok@gmail.com>
2022-11-07 21:49:51 -0600

    cambiar vite por mix en bbb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,12 @@
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
     <link rel="stylesheet" href="{{ asset('vendor/jquery-ui/jquery-ui.min.css') }}">
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    
     <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @livewireStyles
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">
